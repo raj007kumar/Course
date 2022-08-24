2c562830b29e639e8f867726293d5519de10c7fa
raj007kumar <104445552+raj007kumar@users.noreply.github.com>
2022-08-24 22:35:16 +0530

    Add files via upload
    dummy_course3

---
+++ course3.py
@@ -0,0 +1,7 @@
+name = ("raj kumar")
+print(name)
+module_name = ("github.com/modules")
+print(module_name)
+
+print("in this  github repository i learn about how to created this repository data  and save in the database like distributed version control systems so its easily access by other developers from other places.")
+     
